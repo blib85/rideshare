a873074ee756b327fadbd9634eae0546544410f4
Matt Reverzani <matt.reverzani@education.wa.edu.au>
2025-07-03 14:07:53 +0800

    Initial commit

---
+++ app/src/main/java/com/blib85/rideshare/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.blib85.rideshare
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView : WebView = findViewById(R.id.webview_id)
+        myWebView.apply{
+            loadUrl("https://www.uber.com")
+            settings.javaScriptEnabled = true
+            webViewClient = (WebViewClient())
+        }
+    }
+}
